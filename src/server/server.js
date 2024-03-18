e5561615832cf2e9269f95642b1d7e0500af1083
hyub <hyub@hyubui-MacBookPro.local>
2024-03-19 01:36:15 +0900

    server설정

--- src/server/server.js
+++ src/server/server.js
@@ -27,10 +27,6 @@ const allowedOrigins = [
 ];
 app.use(cors({ origin: allowedOrigins }));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 /**
  *
  * post api
@@ -297,4 +293,8 @@ app.post("/deleteImage", (req, res) => {
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "..", "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
